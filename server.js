ee01b8c61a1de1d41c45a961d973013b93f891bd
Benson <phonybenson@gmail.com>
2026-07-25 22:15:58 +0000

    Add EJS project views and partials

--- server.js
+++ server.js
@@ -6,21 +6,10 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-/* below is what will become expanded upon in our make-your-own adventure, right now it is just a home page that leads to another page */
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>AI dev game</h1>
-    <a href="/hello">Go to the next page</a>
-    <a href="/greetings">Go to the next page</a>
-  `);
-});
-
-/*When we start to know what the game will be like, we will make ejs files and switch to the more scalable version below*/
-/*
 app.get("/", (req, res) => {
   res.render("start");
 });
-*/
+
 
 app.get("/hello", (req, res) => {
   res.send("<h1>Hello World!</h1>");
